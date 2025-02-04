d80df0758023d3e7a2b659d54991de042f31a90c
7Shreeju <shreeju.acharya@digihost.in>
2025-02-04 14:01:07 +0530

    Removed all folders from Git but kept them locally

--- server/models/privilege-model.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require("mongoose");
-
-const roleSchema = new Schema({
-    role: {type: String, required: true},
-    createdby:{type: String},
-    user_id:{type:String},
-});
-const Role = new model('Role',roleSchema);
-module.exports = Role;
--- server/router/domain-router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const domaincontrollers = require("../controllers/domain-controller.js");
-
-
-router.route("/getdomaindetails/:id").get(domaincontrollers.getdomaindetails);
-
-module.exports = router;
